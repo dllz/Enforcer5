ba6fcf0f4a7173b06016a3d41e9a28b70c04b36b
Daniel Lotz <daniellotzsa@gmail.com>
2016-12-30 14:10:59 +0200

    Tearing out Werewolf code and building bot framework in .net core

---
+++ src/Enforcer5/Helpers/Bot.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Enforcer5;
+using Microsoft.Win32;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Werewolf_Control.Handler;
+using Werewolf_Control.Models;
+
+namespace Werewolf_Control.Helpers
+{
+    internal static class Bot
+    {
+        internal static string TelegramAPIKey;
+        public static TelegramBotClient Api;
+        public static User Me;
+        public static DateTime StartTime = DateTime.UtcNow;
+        public static bool Running = true;
+        public static long CommandsReceived = 0;
+        public static long MessagesProcessed = 0;
+        public static long MessagesReceived = 0;
+        public static long TotalPlayers = 0;
+        public static long TotalGames = 0;
+        public static Random R = new Random();
+        public static XDocument English;
+        public static int MessagesSent = 0;
+        public static string CurrentStatus = "";
+        internal static string RootDirectory
+        {
+            get
+            {
+                var path = AppContext.BaseDirectory;;
+                return Path.GetDirectoryName(path);
+            }
+        }
+        internal delegate void ChatCommandMethod(Update u, string[] args);
+        internal static List<Command> Commands = new List<Command>();
+        internal static string LanguageDirectory => Path.GetFullPath(Path.Combine(RootDirectory, @"..\..\Languages"));
+        internal static string TempLanguageDirectory => Path.GetFullPath(Path.Combine(RootDirectory, @"..\..\TempLanguageFiles"));
+        public static void Initialize(string updateid = null)
+        {
+
+            //get api token from registry
+            var key =
+                    RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64)
+                        .OpenSubKey("SOFTWARE\\Werewolf");
+            TelegramAPIKey = key.GetValue("EnforcerAPI").ToString();
+            Api = new TelegramBotClient(TelegramAPIKey);
+
+            English = XDocument.Load(Path.Combine(LanguageDirectory, "English.xml"));
+
+            //load the commands list
+            foreach (var m in typeof(Commands).GetMethods())
+            {
+                var c = new Command();
+                foreach (var a in m.GetCustomAttributes(true))
+                {
+                    if (a is Attributes.Command)
+                    {
+                        var ca = a as Attributes.Command;
+                        c.Blockable = ca.Blockable;
+                        c.DevOnly = ca.DevOnly;
+                        c.GlobalAdminOnly = ca.GlobalAdminOnly;
+                        c.GroupAdminOnly = ca.GroupAdminOnly;
+                        c.Trigger = ca.Trigger;
+                        c.Method = (ChatCommandMethod)Delegate.CreateDelegate(typeof(ChatCommandMethod), m);
+                        c.InGroupOnly = ca.InGroupOnly;
+                        Commands.Add(c);
+                    }
+                }
+            }
+
+            Api.OnInlineQuery += UpdateHandler.InlineQueryReceived;
+            Api.OnUpdate += UpdateHandler.UpdateReceived;
+            Api.OnCallbackQuery += UpdateHandler.CallbackReceived;
+            Api.OnReceiveError += ApiOnReceiveError;
+            Me = Api.GetMeAsync().Result;
+
+            Console.Title += " " + Me.Username;
+            StartTime = DateTime.UtcNow;
+            //now we can start receiving
+            Api.StartReceiving();
+        }
+
+        internal static void ReplyToCallback(CallbackQuery query, string text = null, bool edit = true, bool showAlert = false, InlineKeyboardMarkup replyMarkup = null)
+        {
+            //first answer the callback
+            Bot.Api.AnswerCallbackQueryAsync(query.Id, edit ? null : text, showAlert);
+            //edit the original message
+            if (edit)
+                Edit(query, text, replyMarkup);
+        }
+
+        internal static Task<Message> Edit(CallbackQuery query, string text, InlineKeyboardMarkup replyMarkup = null)
+        {
+            return Edit(query.Message.Chat.Id, query.Message.MessageId, text, replyMarkup);
+        }
+
+        internal static Task<Message> Edit(long id, int msgId, string text, InlineKeyboardMarkup replyMarkup = null)
+        {
+            Bot.MessagesSent++;
+            return Bot.Api.EditMessageTextAsync(id, msgId, text, replyMarkup: replyMarkup);
+        }
+
+
+        private static void ApiOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+        {
+            if (!Api.IsReceiving)
+            {
+                Api.StartReceiving();
+            }
+            var e = receiveErrorEventArgs.ApiRequestException;
+            using (var sw = System.IO.File.AppendText(Path.Combine(RootDirectory, "..\\Logs\\apireceiveerror.log")))
+            {
+                sw.WriteLine($"{DateTime.Now} {e.ErrorCode} - {e.Message}\n{e.Source}");
+            }
+                
+        }
+
+        internal static async Task<Message> Send(string message, long id, bool clearKeyboard = false,
+            InlineKeyboardMarkup customMenu = null, ParseMode parseMode = ParseMode.Html)
+        {
+            MessagesSent++;
+            //message = message.Replace("`",@"\`");
+            if (clearKeyboard)
+            {
+                var menu = new ReplyKeyboardHide {HideKeyboard = true};
+                return await Api.SendTextMessageAsync(id, message, replyMarkup: menu, disableWebPagePreview: true,
+                    parseMode: parseMode);
+            }
+            else if (customMenu != null)
+            {
+                return await Api.SendTextMessageAsync(id, message, replyMarkup: customMenu, disableWebPagePreview: true,
+                    parseMode: parseMode);
+            }
+            else
+            {
+                return await Api.SendTextMessageAsync(id, message, disableWebPagePreview: true, parseMode: parseMode);
+            }
+
+        }
+    }
+}
--- src/Enforcer5/Program.cs
+++ src/Enforcer5/Program.cs
@@ -1,14 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Werewolf_Control.Helpers;
 
 namespace Enforcer5
 {
     public class Program
     {
+        internal static bool Running = true;
+        private static bool _writingInfo = false;
+        internal static List<long> MessagesReceived = new List<long>();
+        internal static List<long> MessagesProcessed = new List<long>();
+        internal static List<long> MessagesSent = new List<long>();
+        private static long _previousMessages, _previousMessagesTx, _previousMessagesRx;
+        internal static float MessagePxPerSecond, MessageRxPerSecond, MessageTxPerSecond;
+        internal static int NodeMessagesSent = 0;
+        private static System.Threading.Timer _timer;
+        public static DateTime MaxTime = DateTime.MinValue;
         public static void Main(string[] args)
         {
+            Console.Title = "Enforcer";
+
+            //Make sure another instance isn't already running
+            if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1)
+            {
+                Environment.Exit(2);
+            }
+
+            new Thread(() => Bot.Initialize()).Start();
+
+            //new Thread(UpdateHandler.SpamDetection).Start();
+            //new Thread(UpdateHandler.BanMonitor).Start();
+            _timer = new Timer(TimerOnTick, null, 5000, 1000);
+            //now pause the main thread to let everything else run
+            Thread.Sleep(-1);
+        }
+
+        private static void TimerOnTick(Object stateInfo)
+        {
+            try
+            {
+                var newMessages = Bot.MessagesProcessed - _previousMessages;
+                _previousMessages = Bot.MessagesProcessed;
+                MessagesProcessed.Insert(0, newMessages);
+                if (MessagesProcessed.Count > 60)
+                    MessagesProcessed.RemoveAt(60);
+                MessagePxPerSecond = MessagesProcessed.Max();
+
+                newMessages = (Bot.MessagesSent + NodeMessagesSent) - _previousMessagesTx;
+                _previousMessagesTx = (Bot.MessagesSent + NodeMessagesSent);
+                MessagesSent.Insert(0, newMessages);
+                if (MessagesSent.Count > 60)
+                    MessagesSent.RemoveAt(60);  
+                MessageTxPerSecond = MessagesSent.Max();
+
+                newMessages = Bot.MessagesReceived - _previousMessagesRx;
+                _previousMessagesRx = Bot.MessagesReceived;
+                MessagesReceived.Insert(0, newMessages);
+                if (MessagesReceived.Count > 60)
+                    MessagesReceived.RemoveAt(60);
+                MessageRxPerSecond = MessagesProcessed.Max();
+            }
+            catch
+            {
+                // ignored
+            }
         }
     }
 }
