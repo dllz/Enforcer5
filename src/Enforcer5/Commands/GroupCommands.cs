4aa071b019ddf07252cfee7a805d1d709d95f6d4
Daniel Lotz <daniellotzsa@gmail.com>
2017-01-25 15:21:36 +0200

    About command

--- src/Enforcer5/Commands/GroupCommands.cs
+++ src/Enforcer5/Commands/GroupCommands.cs
@@ -25,7 +25,7 @@ namespace Enforcer5
             }
         }
 
-        [Command(Trigger = "setrules", InGroupOnly = true)]
+        [Command(Trigger = "setrules", InGroupOnly = true, GroupAdminOnly = true)]
         public static async void SetRules(Update update, string[] args)
         {
             var lang = Methods.GetGroupLanguage(update.Message).Doc;
@@ -49,6 +49,47 @@ namespace Enforcer5
                 await Bot.Send(Methods.GetLocaleString(lang, "NoInput"), update);
             }
         }
-             
+
+        [Command(Trigger = "about", InGroupOnly = true)]
+        public static async void About(Update update, string[] args)
+        {
+            var lang = Methods.GetGroupLanguage(update.Message).Doc;
+            var chatId = update.Message.Chat.Id;
+            var text = Methods.GetAbout(chatId, lang);
+            if (Methods.SendInPm(update.Message, "Rules"))
+            {
+                await Bot.Send(text, update.Message.From.Id);
+            }
+            else
+            {
+                await Bot.Send(text, update);
+            }
+        }
+
+
+        [Command(Trigger = "setabout", InGroupOnly = true, GroupAdminOnly = true)]
+        public static async void SetAbout(Update update, string[] args)
+        {
+            var lang = Methods.GetGroupLanguage(update.Message).Doc;
+            if (!string.IsNullOrWhiteSpace(args[1]))
+            {
+                var input = args[1];
+                try
+                {
+                    var result = Bot.Send(input, update);
+                    Redis.db.StringSet($"chat:{update.Message.Chat.Id}:about", input);
+                    await Bot.Api.EditMessageTextAsync(update.Message.Chat.Id, result.Result.MessageId,
+                        Methods.GetLocaleString(lang, "AboutSet"));
+                }
+                catch (AggregateException e)
+                {
+                    Methods.SendError(e.InnerExceptions[0], update.Message, lang);
+                }
+            }
+            else
+            {
+                await Bot.Send(Methods.GetLocaleString(lang, "NoInput"), update);
+            }
+        }
     }
 }
