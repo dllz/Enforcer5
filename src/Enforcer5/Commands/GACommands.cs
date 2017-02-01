06548a7843c2539be486fc4e3afa7380a8f4b02a
Daniel Lotz <daniellotzsa@gmail.com>
2017-02-01 22:16:31 +0200

    reload lang command

--- src/Enforcer5/Commands/GACommands.cs
+++ src/Enforcer5/Commands/GACommands.cs
@@ -162,6 +162,13 @@ namespace Enforcer5
             var res =  Redis.db.SetRemoveAsync("langAdmins", id).Result;
             await Bot.SendReply("Done", update);
         }
+        [Command(Trigger = "reloadLang", RequiresReply = true, UploadAdmin = true)]
+        public static async Task reloadLang(Update update, string[] args)
+        {
+            Methods.IntialiseLanguages();
+            await Bot.SendReply("Done", update);
+        }
+
     }
 
     public static partial class CallBacks
