a15aa2ccccde8a1808cd8f6e6dfb9169c867b7b2
Daniel Lotz <daniellotzsa@gmail.com>
2017-02-02 10:16:39 +0200

    fixed on message parse bug

--- src/Enforcer5/Commands/OnMessage.cs
+++ src/Enforcer5/Commands/OnMessage.cs
@@ -23,46 +23,50 @@ namespace Enforcer5
                 if (!isIgnored(chatId, msgType))
                 {
                     var msgs = Redis.db.StringGetAsync($"spam:{chatId}:{update.Message.From.Id}").Result;
-                    var num = msgs.IsInteger ? int.Parse(msgs) : 0;
+                    int num = msgs.IsInteger ? int.Parse(msgs) : 0;
                     if (num == 0) num = 1;
                     var maxSpam = 8;
                     if (update.Message.Chat.Type == ChatType.Private) maxSpam = 12;
                     var floodSettings = Redis.db.HashGetAllAsync($"chat:{chatId}:flood").Result;
-                    var maxMsgs = floodSettings.Where(e => e.Name.Equals("MaxFlood")).FirstOrDefault();
+                    var maxMsgs = floodSettings.Where(e => e.Name.Equals("MaxFlood")).FirstOrDefault();                   
                     var maxTime = new TimeSpan(0, 0, 0, 5);
+                    int maxmsgs;
                     if ((DateTime.Now.ToUnixTime() - update.Message.Date.ToUnixTime()) < 30)
                     {
                         await Redis.db.StringSetAsync($"spam:{chatId}:{update.Message.From.Id}", num + 1, maxTime);
                     }
-                    if (num == int.Parse(maxMsgs.Value) + 1)
+                    if (int.TryParse(maxMsgs.Value, out maxmsgs))
                     {
-                        var action = floodSettings.Where(e => e.Name.Equals("ActionFlood")).FirstOrDefault();
-                        var name = update.Message.From.FirstName;
-                        if (update.Message.From.Username != null) name = $"{name} (@{update.Message.From.Username})";
-                        try
+                        if (num == (int.Parse(maxMsgs.Value) + 1))
                         {
-                            if (action.Value.Equals("ban"))
+                            var action = floodSettings.Where(e => e.Name.Equals("ActionFlood")).FirstOrDefault();
+                            var name = update.Message.From.FirstName;
+                            if (update.Message.From.Username != null) name = $"{name} (@{update.Message.From.Username})";
+                            try
                             {
-                                await Methods.BanUser(chatId, update.Message.From.Id, lang);
-                                Methods.SaveBan(update.Message.From.Id, "flood");
-                                Methods.AddBanList(chatId, update.Message.From.Id, update.Message.From.FirstName,
-                                    Methods.GetLocaleString(lang, "bannedForFlood", ""));
+                                if (action.Value.Equals("ban"))
+                                {
+                                    await Methods.BanUser(chatId, update.Message.From.Id, lang);
+                                    Methods.SaveBan(update.Message.From.Id, "flood");
+                                    Methods.AddBanList(chatId, update.Message.From.Id, update.Message.From.FirstName,
+                                        Methods.GetLocaleString(lang, "bannedForFlood", ""));
+                                }
+                                else
+                                {
+                                    await Methods.KickUser(chatId, update.Message.From.Id, lang);
+                                    Methods.SaveBan(update.Message.From.Id, "flood");
+                                }
+                                if (msgs == (int.Parse(maxMsgs.Value) + 1) || msgs == int.Parse(maxMsgs.Value) + 5)
+                                {
+                                    await Bot.Send(
+                                        Methods.GetLocaleString(lang, "bannedForFlood", $"{update.Message.From.FirstName}, {update.Message.From.Id}"),
+                                        update);
+                                }
                             }
-                            else
+                            catch (Exception e)
                             {
-                                await Methods.KickUser(chatId, update.Message.From.Id, lang);
-                                Methods.SaveBan(update.Message.From.Id, "flood");
-                            }
-                            if (msgs == (int.Parse(maxMsgs.Value) + 1) || msgs == int.Parse(maxMsgs.Value) + 5)
-                            {
-                                await Bot.Send(
-                                    Methods.GetLocaleString(lang, "bannedForFlood", $"{update.Message.From.FirstName}, {update.Message.From.Id}"),
-                                    update);
-                            }
-                        }
-                        catch (Exception e)
-                        {
 
+                            }
                         }
                     }
                 }
