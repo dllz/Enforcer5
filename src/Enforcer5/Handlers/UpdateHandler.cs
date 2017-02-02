ff6ea0ee9fa3ca2563b6452ab52c3424d29b6689
Daniel Lotz <daniellotzsa@gmail.com>
2017-02-03 00:16:50 +0200

    raised spam detection

--- src/Enforcer5/Handlers/UpdateHandler.cs
+++ src/Enforcer5/Handlers/UpdateHandler.cs
@@ -581,17 +581,17 @@ namespace Enforcer5.Handlers
                             //    continue;
                             //}
                             //now count, notify if limit hit
-                            if (temp[key].Messages.Count() >= 5) // 20 in a minute
+                            if (temp[key].Messages.Count() >= 10) // 20 in a minute
                             {
                                 temp[key].Warns++;
-                                if (temp[key].Warns < 2 && temp[key].Messages.Count < 10)
+                                if (temp[key].Warns < 2 && temp[key].Messages.Count < 15)
                                 {
                                     Send($"Please do not spam me. Next time is automated ban.", key);
                                     //Send($"User {key} has been warned for spamming: {temp[key].Warns}\n{temp[key].Messages.GroupBy(x => x.Command).Aggregate("", (a, b) => a + "\n" + b.Count() + " " + b.Key)}",
                                     //    Para);
                                     continue;
                                 }
-                                if ((temp[key].Warns >= 3 || temp[key].Messages.Count >= 15) & !temp[key].NotifiedAdmin)
+                                if ((temp[key].Warns >= 3 || temp[key].Messages.Count > 15) & !temp[key].NotifiedAdmin)
                                 {
                                     Redis.db.StringSet($"spammers{key}", key, TimeSpan.FromMinutes(10));
                                     Bot.Send("You have been banned for 10 minutes due to spam", long.Parse(key.ToString()));
