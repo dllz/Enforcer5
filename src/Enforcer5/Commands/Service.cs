8f83a9bb8a711e73a726fa26c824ecae55bb4aa2
Daniel Lotz <daniellotzsa@gmail.com>
2017-02-03 14:53:54 +0200

    welcome spam

--- src/Enforcer5/Commands/Service.cs
+++ src/Enforcer5/Commands/Service.cs
@@ -47,7 +47,7 @@ namespace Enforcer5
             var joinSpam = Redis.db.StringGetAsync($"spam:added:{message.Chat.Id}:{message.NewChatMember.Id}").Result;
             defSpamValue = 3;
             maxTime = TimeSpan.FromMinutes(5);
-            if (msgs.HasValue && !string.IsNullOrEmpty(msgs))
+            if (joinSpam.HasValue && !string.IsNullOrEmpty(joinSpam))
             {
                 try
                 {
@@ -76,7 +76,7 @@ namespace Enforcer5
             joinSpam = Redis.db.StringGetAsync($"spam:added:{message.NewChatMember.Id}").Result;
             defSpamValue = 3;
             maxTime = TimeSpan.FromMinutes(5);
-            if (msgs.HasValue && !string.IsNullOrEmpty(msgs))
+            if (joinSpam.HasValue && !string.IsNullOrEmpty(joinSpam))
             {
                 try
                 {
