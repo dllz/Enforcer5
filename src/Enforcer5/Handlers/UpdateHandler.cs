563eefb01f7176092d37488653ae2e59d9ffb41c
Daniel Lotz <daniellotzsa@gmail.com>
2017-03-02 19:01:50 +0200

    removed enforcer p

--- src/Enforcer5/Handlers/UpdateHandler.cs
+++ src/Enforcer5/Handlers/UpdateHandler.cs
@@ -79,11 +79,11 @@ namespace Enforcer5.Handlers
         private static async void HandleUpdate(Update update)
         {
             {
-#if premium
-                var allowed = Redis.db.SetContainsAsync("premiumBot", update.Message.Chat.Id).Result;
-                if (!allowed)
-                    return;
-#endif
+//#if premium
+//                var allowed = Redis.db.SetContainsAsync("premiumBot", update.Message.Chat.Id).Result;
+//                if (!allowed)
+//                    return;
+//#endif
                 new Task(() => { CollectStats(update.Message); }).Start();                
                 Bot.MessagesProcessed++;
                 Methods.IsRekt(update);
