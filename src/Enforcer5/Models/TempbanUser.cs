154d95e80c260b940f64ae426758bec4364f8f61
dllz <daniellotzsa@gmail.com>
2017-06-11 15:36:33 +0200

    another missing model

---
+++ src/Enforcer5/Models/TempbanUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Enforcer5.Models
+{
+    public class TempbanUser
+    {
+        public string name { get; set; }
+        public string unbanTime { get; set; }
+        public string group { get; set; }
+    }
+}
